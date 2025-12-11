85b7b9e55081599998e90cea047ee3834f9c1884
Joe Cisneros <jmc@asu.edu>
2025-12-11 15:36:10 -0700

    remove options

--- src/Episodes/Services/Tmdb/TmdbClient.cs
+++ src/Episodes/Services/Tmdb/TmdbClient.cs
@@ -1,19 +1,14 @@
-using Episodes.Config;
-using Microsoft.Extensions.Options;
-
 namespace Episodes.Services.Tmdb;
 
 public class TmdbClient : ITmdbClient
 {
     private readonly HttpClient _client;
-    private readonly TmdbOptions _options;
     private readonly ILogger<TmdbClient> _logger;
 
-    public TmdbClient(IOptions<TmdbOptions> options, HttpClient client, ILogger<TmdbClient> logger)
+    public TmdbClient(HttpClient client, ILogger<TmdbClient> logger)
     {
         _client = client;
         _logger = logger;
-        _options = options.Value;
     }
     
     public Task<TmdbSearchTvResponse> SearchTvShowsAsync(string query, CancellationToken token = default)
