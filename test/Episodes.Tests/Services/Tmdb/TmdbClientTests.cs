73eaaadf786abd5b523a2245ffcc7d948adc00c2
Joe Cisneros <jmc@asu.edu>
2026-01-20 23:19:41 -0700

    fill out TmdbClientTests

--- test/Episodes.Tests/Services/Tmdb/TmdbClientTests.cs
+++ test/Episodes.Tests/Services/Tmdb/TmdbClientTests.cs
@@ -1,6 +1,182 @@
+using System.Net;
+using System.Text;
+using Episodes.Services.Tmdb;
+using FluentAssertions;
+using NUnit.Framework;
+
 namespace Episodes.Tests.Services.Tmdb;
 
+[TestFixture]
 public class TmdbClientTests
 {
-    
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("   ")]
+    public async Task SearchTvShowsAsync_WhenQueryIsNullOrWhitespace_ThrowsArgumentException(string? query)
+    {
+        // Arrange
+        var handler = new StubHttpMessageHandler(new HttpResponseMessage(HttpStatusCode.OK));
+        var httpClient = CreateHttpClient(handler);
+        var sut = new TmdbClient(httpClient);
+
+        // Act
+        var act = async () => await sut.SearchTvShowsAsync(query!);
+
+        // Assert
+        var ex = await act.Should().ThrowAsync<ArgumentException>();
+        ex.Which.ParamName.Should().Be("query");
+    }
+
+    [Test]
+    public async Task SearchTvShowsAsync_WhenSuccessful_ReturnsResponse()
+    {
+        // Arrange
+        const string query = "game of thrones";
+        var json = """
+                   {
+                     "page": 1,
+                     "results": [{ "id": 1399, "name": "Game of Thrones" }],
+                     "total_pages": 1,
+                     "total_results": 1
+                   }
+                   """;
+        var handler = new StubHttpMessageHandler(
+            new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            });
+        var httpClient = CreateHttpClient(handler);
+        var sut = new TmdbClient(httpClient);
+
+        // Act
+        var result = await sut.SearchTvShowsAsync(query);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Page.Should().Be(1);
+        result.TotalPages.Should().Be(1);
+        result.TotalResults.Should().Be(1);
+        result.Results.Should().Contain(x => x.Id == 1399);
+    }
+
+    [Test]
+    public async Task SearchTvShowsAsync_WhenHttpError_ThrowsException()
+    {
+        // Arrange
+        var handler = new StubHttpMessageHandler(
+            new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent("""{"error":"bad request"}""")
+            });
+        var httpClient = CreateHttpClient(handler);
+        var sut = new TmdbClient(httpClient);
+
+        // Act
+        var act = async () => await sut.SearchTvShowsAsync("bad");
+
+        // Assert
+        await act.Should().ThrowAsync<TmdbApiException>();
+    }
+
+    [Test]
+    public async Task GetTvShowDetailsAsync_WhenSuccessful_ReturnsResponse()
+    {
+        // Arrange
+        var json = """
+                   {
+                     "id": 1399,
+                     "name": "Game of Thrones",
+                     "number_of_seasons": 8
+                   }
+                   """;
+        var handler = new StubHttpMessageHandler(
+            new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            });
+        var httpClient = CreateHttpClient(handler);
+        var sut = new TmdbClient(httpClient);
+
+        // Act
+        var result = await sut.GetTvShowDetailsAsync(1399);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(1399);
+        result.Name.Should().Be("Game of Thrones");
+        result.NumberOfSeasons.Should().Be(8);
+    }
+
+    [Test]
+    public async Task GetTvShowDetailsAsync_WhenHttpError_ThrowsException()
+    {
+        // Arrange
+        var handler = new StubHttpMessageHandler(
+            new HttpResponseMessage(HttpStatusCode.NotFound)
+            {
+                Content = new StringContent("""{"error":"not found"}""")
+            });
+
+        var httpClient = CreateHttpClient(handler);
+        var sut = new TmdbClient(httpClient);
+
+        // Act
+        var act = async () => await sut.GetTvShowDetailsAsync(999);
+
+        // Assert
+        await act.Should().ThrowAsync<TmdbApiException>();
+    }
+
+    [Test]
+    public async Task GetTvShowSeasonDetailsAsync_WhenSuccessful_ReturnsResponse()
+    {
+        // Arrange
+        var json = """
+                   {
+                     "id": 1,
+                     "name": "Season 1",
+                     "season_number": 1,
+                     "episodes": []
+                   }
+                   """;
+        var handler = new StubHttpMessageHandler(
+            new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            });
+        var httpClient = CreateHttpClient(handler);
+        var sut = new TmdbClient(httpClient);
+
+        // Act
+        var result = await sut.GetTvShowSeasonDetailsAsync(1399, 1);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(1);
+        result.Name.Should().Be("Season 1");
+        result.SeasonNumber.Should().Be(1);
+    }
+
+    private static HttpClient CreateHttpClient(HttpMessageHandler handler)
+    {
+        return new HttpClient(handler)
+        {
+            BaseAddress = new Uri("https://api.themoviedb.org")
+        };
+    }
+
+    private sealed class StubHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly HttpResponseMessage _response;
+
+        public StubHttpMessageHandler(HttpResponseMessage response)
+        {
+            _response = response;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+            => Task.FromResult(_response);
+    }
 }
