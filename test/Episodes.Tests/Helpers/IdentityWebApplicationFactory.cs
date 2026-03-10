c33cf43c3db2a261638e600725c452e483621c40
Joe Cisneros <jmc@asu.edu>
2026-03-09 21:48:10 -0700

    fix integration tests

--- test/Episodes.Tests/Helpers/IdentityWebApplicationFactory.cs
+++ test/Episodes.Tests/Helpers/IdentityWebApplicationFactory.cs
@@ -1,23 +1,40 @@
 using System.Data.Common;
 using Episodes.Data;
+using Episodes.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using NSubstitute;
 
 namespace Episodes.Tests.Helpers;
 
-public sealed class IdentityWebApplicationFactory : EpisodesWebApplicationFactory
+public sealed class IdentityWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private ITvShowService TvShowService { get; } = Substitute.For<ITvShowService>();
+
     private SqliteConnection? _connection;
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        base.ConfigureWebHost(builder);
+        builder.UseEnvironment("Development");
+
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:DefaultConnection"] =
+                    "Host=127.0.0.1;Port=1;Database=test;Username=test;Password=test;Timeout=1;Command Timeout=1",
+                ["Tmdb:BaseUrl"] = "https://api.themoviedb.org",
+                ["Tmdb:ApiToken"] = "test-token"
+            });
+        });
 
         builder.ConfigureTestServices(services =>
         {
+            services.AddSingleton(TvShowService);
+
             services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
             services.RemoveAll<ApplicationDbContext>();
 
@@ -38,15 +55,9 @@ public sealed class IdentityWebApplicationFactory : EpisodesWebApplicationFactor
         var host = base.CreateHost(builder);
 
         using var scope = host.Services.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        dbContext.Database.EnsureCreated();
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.EnsureCreated();
 
         return host;
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        base.Dispose(disposing);
-        _connection?.Dispose();
     }
 }
